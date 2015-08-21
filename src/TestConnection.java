546ac44f44a422d90b717ae3e577fee28ebcbdc0
unknown <alok.kumar@IITC-WS-JAVA03.Intelegencia.com>
2015-08-21 15:18:39 +0530

    no message

---
+++ src/TestConnection.java
@@ -0,0 +1,32 @@
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+
+public class TestConnection {
+
+	public static void main(String[] args) {
+		Connection conn = null;
+		try{
+			System.out.println("con...1");
+		    Class.forName("oracle.jdbc.driver.OracleDriver");  
+   	  
+     	//step2 create  the connection object  
+     	conn=DriverManager.getConnection("jdbc:oracle:thin:@ecom.intelegencia.com:1521:orcl","ecom1","ecom1");  
+		System.out.println("con..."+conn);
+		PreparedStatement ps=conn.prepareStatement("SELECT * FROM WSO2");
+        ResultSet rs = ps.executeQuery();
+        
+    	while(rs.next()){
+    	 System.out.println("rs......."+rs.getString("name"));
+    	} 
+		}
+		catch(Exception ex)
+		{}
+
+	}
+
+}
